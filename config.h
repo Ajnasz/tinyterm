db46abb57480de2955af737814edf0bb26c9bdfd
Lajos Koszti <ajnasz@ajnasz.hu>
2016-05-02 21:23:26 +0200

    Move colors to config_colors.h

--- config.h
+++ config.h
@@ -37,46 +37,7 @@
 #define TINYTERM_FONT           "Envy Code R 12"
 #define TINYTERM_STYLE "GtkWindow { background: black; }"
 
-
-#define base00 "#000000"
-#define base01 "#3a3432"
-#define base02 "#4a4543"
-#define base03 "#5c5855"
-#define base04 "#807d7c"
-#define base05 "#a5a2a2"
-#define base06 "#dddddd"
-#define base07 "#f7f7f7"
-#define base08 "#db2d20"
-#define base09 "#e8bbd0"
-#define base0A "#fded02"
-#define base0B "#01a252"
-// #define base0C "#b5e4f4"
-#define base0C "#91E3FF"
-#define base0D "#01a0e4"
-//#define base0E "#a16a94"
-#define base0E "#D157DE"
-#define base0F "#cdab53"
-
-#define TINYTERM_COLOR_FOREGROUND base07
-#define TINYTERM_COLOR_BACKGROUND base00
-#define TINYTERM_COLOR00 base00
-#define TINYTERM_COLOR01 base08
-#define TINYTERM_COLOR02 base0B
-#define TINYTERM_COLOR03 base0A
-#define TINYTERM_COLOR04 base0D
-#define TINYTERM_COLOR05 base0E
-#define TINYTERM_COLOR06 base0C
-#define TINYTERM_COLOR07 base06
-
-#define TINYTERM_COLOR08 base00
-#define TINYTERM_COLOR09 base08
-#define TINYTERM_COLOR0A base0B
-#define TINYTERM_COLOR0B base0A
-#define TINYTERM_COLOR0C base0D
-#define TINYTERM_COLOR0D base0E
-#define TINYTERM_COLOR0E base0C
-#define TINYTERM_COLOR0F base06
-
+#include "config_colors.h"
 /* One of VTE_CURSOR_SHAPE_BLOCK, VTE_CURSOR_SHAPE_IBEAM, VTE_CURSOR_SHAPE_UNDERLINE */
 #define TINYTERM_CURSOR_SHAPE   VTE_CURSOR_SHAPE_BLOCK
 
